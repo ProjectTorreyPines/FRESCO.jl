fa65bee2d1033862111396db8b513bd4264e610f
Brendan Lyons <lyonsbc@fusion.gat.com>
2024-08-09 14:49:42 -0700

    WIP: Create initial iterative solve workflow

--- src/FRESCO.jl
+++ src/FRESCO.jl
@@ -11,6 +11,7 @@ import Interpolations
 using PolygonOps: inpolygon, centroid
 import HypergeometricFunctions: _₂F₁ as F21
 using RecipesBase
+using Plots
 
 const μ₀ = π * 4e-7
 const twopi = 2π
@@ -20,10 +21,12 @@ include("flux.jl")
 include("GSsolve.jl")
 include("current.jl")
 include("feedback.jl")
+include("workflow.jl")
 
 export Canvas
 export flux, psinorm
 export invert_GS!, invert_GS_zero_bnd!
 export initial_current
+export solve!
 
 end
